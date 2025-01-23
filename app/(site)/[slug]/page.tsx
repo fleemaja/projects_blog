f36aa9e638b61e2da6d1a0e2df147f009210a17e
Drew Fleeman <drew.fleeman@gmail.com>
2025-01-23 16:58:45 -0600

    add content to 404 not found page

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { PortableText } from "@portabletext/react"
 import About from "./about";
 import Books from "./books";
+import Link from "next/link";
 
 type Props = {
     params: { slug: string }
@@ -18,10 +19,16 @@ export default function Page({params}: Props) {
         )
     } else {
         return (
-            <div className="max-w-3xl mx-auto mt-20 px-5 lg:px-0">
-                <h1 className="text-gray-700 text-9xl font-extrabold">
-                    404
+            <div className="grid content-center max-w-3xl mx-auto mt-20 px-5 lg:px-0">
+                <h1 className="mx-auto text-gray-700 text-9xl font-bold">
+                  (&gt;_&lt;)
                 </h1>
+                <h2 className="mx-auto my-10">This page does not exist.</h2>
+                <Link href="/" className="mx-auto" >
+                  <span className="ml-2 underline underline-offset-8 decoration-blue-500">
+                    Return to Drew's Projects
+                  </span>
+                </Link>
             </div>
         )
     }
